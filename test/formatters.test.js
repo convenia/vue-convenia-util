e08ea071a1852a0fdfdd5cf20dfb737c32ad8baf
Gui Barscevicius <guilherme.barscevicius@convenia.com.br>
2019-05-03 10:34:15 -0300

    fix tests

--- test/formatters.test.js
+++ test/formatters.test.js
@@ -39,8 +39,8 @@ test('toMoney: Não formata valores inválidos', (context) => {
 })
 
 test('toYears: Obtém em anos a diferença de uam data', (context) => {
-  context.is(format.toYears('28/03/1996'), 21)
-  context.is(format.toYears('1979-04-13'), 38)
+  context.is(format.toYears('28/03/1996'), 23)
+  context.is(format.toYears('1979-04-13'), 40)
 })
 
 test('toYears: Não formata valores inválidos', (context) => {
